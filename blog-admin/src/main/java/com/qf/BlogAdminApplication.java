2da423beafbf7f67cf2d26598644cb2ab68a0e40
清风 <1752597830@qq.com>
2023-10-24 23:22:47 +0800

    Blog新项目搭建

---
+++ blog-admin/src/main/java/com/qf/BlogAdminApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+/**
+ * Hello world!
+ *
+ */
+public class BlogAdminApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ blog-front/src/main/java/com/qf/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+/**
+ * Hello world!
+ *
+ */
+public class BlogApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
